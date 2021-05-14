f21a656dba6c94aa01429185bc6b692523ebc7d1
Boeun <boeun0904@naver.com>
2021-05-15 00:16:13 +0900

    기분 서버 연결

--- app/src/main/java/com/example/osproject/EmotionFrag.java
+++ app/src/main/java/com/example/osproject/EmotionFrag.java
@@ -1,7 +1,11 @@
 package com.example.osproject;
 
-
+import android.app.ProgressDialog;
+import android.app.VoiceInteractor;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,16 +14,43 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import androidx.annotation.IntegerRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
 
 public class EmotionFrag extends Fragment {
     private View view;
     private RadioGroup rg_emotions;
     private RadioButton rb_excited, rb_happy, rb_soso, rb_sad, rb_angry;
     private Button bt_camera;
-    private int sel_emotion = -1;
+    private int feelingId = 0;
+
+    String url = "https://dev.evertime.shop/feeling/:num";
+
 
     @Nullable
     @Override
@@ -34,20 +65,55 @@ public class EmotionFrag extends Fragment {
         rb_angry = view.findViewById(R.id.rb_angry);
         //bt_camera = view.findViewById(R.id.bt_camera);
 
+
+
         rg_emotions.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if(checkedId == R.id.rb_excited) sel_emotion = 1;
-                else if(checkedId == R.id.rb_happy) sel_emotion = 2;
-                else if(checkedId == R.id.rb_soso) sel_emotion = 3;
-                else if(checkedId == R.id.rb_sad) sel_emotion = 4;
-                else if(checkedId == R.id.rb_angry) sel_emotion = 5;
 
-                //Toast.makeText(getActivity(),Integer.toString(sel_emotion),Toast.LENGTH_SHORT).show();
-                //sel_emotion으로 emotion정보 반환
+                if(checkedId == R.id.rb_excited) feelingId = 1;
+                else if(checkedId == R.id.rb_happy) feelingId = 2;
+                else if(checkedId == R.id.rb_soso) feelingId = 3;
+                else if(checkedId == R.id.rb_sad) feelingId = 4;
+                else if(checkedId == R.id.rb_angry) feelingId = 5;
+
+                //Toast.makeText(getActivity(),Integer.toString(feelingId),Toast.LENGTH_SHORT).show();
+                //feelingId으로 emotion정보 반환
+                postData();
             }
         });
-
         return view;
     }
+
+    public void postData(){
+        String url="https://dev.evertime.shop/feeling/:num";
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        JSONObject object = new JSONObject();
+        try {
+            object.put("num",feelingId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                object,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("String Response:",response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error getting response:",error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+    }
+
 }
