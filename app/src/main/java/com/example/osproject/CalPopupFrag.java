bc619a80b3681ff678107c414fed6b8ef2d4de14
Boeun <boeun0904@naver.com>
2021-05-16 17:07:50 +0900

    캘린더 팝업창 기본 구현

---
+++ app/src/main/java/com/example/osproject/CalPopupFrag.java
@@ -0,0 +1,26 @@
+package com.example.osproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class CalPopupFrag extends DialogFragment {
+    private View view;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.calendar_popuplist,container,false);
+        return view;
+    }
+
+}
