8e2fd74fdea30fdbb5e26bf788ce2b0feb77a8d4
JoHyunKim <kimjohyun298@gmail.com>
2021-05-18 16:06:28 +0900

    날씨UI수정+감정창 플레이리스트 추가

--- app/src/main/java/com/example/osproject/WeatherFrag.java
+++ app/src/main/java/com/example/osproject/WeatherFrag.java
@@ -1,6 +1,8 @@
 package com.example.osproject;
 
 import android.accessibilityservice.AccessibilityService;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -24,6 +26,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonObject;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,19 +35,24 @@ import org.json.JSONObject;
 //import okhttp3.Request;
 //import okhttp3.Response;
 
+import java.time.Instant;
+
 import static android.content.Context.LOCATION_SERVICE;
 
 public class WeatherFrag extends Fragment {
     private View view;
 
     TextView loc, con, temp, max_temp, min_temp, main_text, des_text;
-    //ImageView pic;
+    ImageView pic;
+
+    TextView title, singer;
 
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.weather,container,false);
 
+        //날씨 위젯
         loc = (TextView)view.findViewById(R.id.mLocationTextView);
         con = (TextView)view.findViewById(R.id.mCountryTextView);
         temp = (TextView)view.findViewById(R.id.mTempTextView);
@@ -53,10 +61,15 @@ public class WeatherFrag extends Fragment {
         main_text = (TextView)view.findViewById(R.id.mMainTextView);
         des_text = (TextView)view.findViewById(R.id.mDescriptionTextView);
 
-        //pic = (ImageView)view.findViewById(R.id.weather_pic);
+        pic = (ImageView)view.findViewById(R.id.weather_pic);
 
         find_weather();
 
+        //날씨 기반 음악 추천
+        title = (TextView)view.findViewById(R.id.song_title);
+        singer = (TextView)view.findViewById(R.id.song_singer);
+
+        find_weatherbase_music();
 
         return view;
     }
@@ -76,37 +89,27 @@ public class WeatherFrag extends Fragment {
 
                     String location = result_object.getString("name");
                     String country = system_object.getString("country");
-                    String temperature = String.valueOf(main_object.getDouble("temp"));
-                    String maxtemp = String.valueOf(main_object.getDouble("temp_max"));
-                    String mintemp = String.valueOf(main_object.getDouble("temp_min"));
+                    Double temperature = main_object.getDouble("temp");
+                    Double maxtemp = main_object.getDouble("temp_max");
+                    Double mintemp = main_object.getDouble("temp_min");
                     String maintx = weather.getString("main");
                     String desttx = weather.getString("description");
 
-                    loc.setText(location);
+                    loc.setText(location + " ,     ");
                     con.setText(country);
-                    temp.setText(temperature);
-                    max_temp.setText(maxtemp);
-                    min_temp.setText(mintemp);
+                    //temp.setText(temperature);
+                    temp.setText(String.valueOf((int)(temperature-273.15)));
+                    max_temp.setText(String.valueOf((int)(maxtemp-273.15)));
+                    min_temp.setText(String.valueOf((int)(mintemp-273.15)));
                     main_text.setText(maintx);
                     des_text.setText(desttx);
 
-//                    double temp_int = Double.parseDouble(String.valueOf(temp));
-//                    double centi_temp_int = (temp_int - 32) / 1.80000;
-//                    centi_temp_int = Math.round(centi_temp_int);
-//                    int i = (int)centi_temp_int;
-//                    temp.setText(String.valueOf(i));
-//
-//                    double max_temp_int = Double.parseDouble(String.valueOf(max_temp));
-//                    double centi_max_temp_int = (max_temp_int - 32) / 1.80000;
-//                    centi_max_temp_int = Math.round(centi_max_temp_int);
-//                    int ii = (int)centi_max_temp_int;
-//                    temp.setText(String.valueOf(ii));
-//
-//                    double min_temp_int = Double.parseDouble(String.valueOf(min_temp));
-//                    double centi_min_temp_int = (min_temp_int - 32) / 1.80000;
-//                    centi_min_temp_int = Math.round(centi_min_temp_int);
-//                    int iii = (int)centi_min_temp_int;
-//                    temp.setText(String.valueOf(iii));
+//                    String icon = weather.getString("icon");
+//                    //String iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
+//                    String iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+//                    Picasso.get().load(iconUrl).into(pic);
+
+                    //.setImageDrawable(R.drawable.);
 
 
                 }catch(JSONException e)
@@ -127,4 +130,35 @@ public class WeatherFrag extends Fragment {
 
     }
 
+    public void find_weatherbase_music()
+    {
+        String url = "https://dev.evertime.shop/weather/music";
+
+        JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try{
+                    JSONObject result_object = response.getJSONObject("result");
+                    String tit = result_object.getString("musicName");
+                    String sin = result_object.getString("singer");
+
+                    title.setText(tit);
+                    singer.setText(sin);
+
+                }catch(JSONException e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }
+        );
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        queue.add(jor);
+    }
 }
